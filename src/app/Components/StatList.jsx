0854fa3b12ae5e07e7c30c1ad72c8335cd884371
Joy <mmehedihasanjoyv@gmail.com>
2023-09-25 21:58:06 +0600

    created the statList

---
+++ src/app/Components/StatList.jsx
@@ -0,0 +1,60 @@
+'use client'
+import React, { useEffect, useState } from 'react';
+
+const StatList = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch("https://agency.teamrabbil.com/api/StatList")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data,'statList');
+                setData(data)
+            })
+    },[])
+    return (
+        <div className='px-[128px] py-[67px]'>
+            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+            <div className="card bg-base-100 shadow-xl w-[250px] h-[250px]">
+                <figure className="p-7 bg-green-100 w-[100px] h-[100px] mx-auto rounded-lg">
+              <img src="https://cdn.icon-icons.com/icons2/154/PNG/512/men_22005.png" alt="Shoes" className="rounded-xl" />
+                     </figure>
+                <div className="card-body items-center text-center">
+                    <p className='font-serif text-3xl'>{ data?.followers}</p>
+                    <h3 className='font-bold'>followers</h3>
+                         </div>
+                </div>
+                <div className="card bg-base-100 shadow-xl w-[250px] h-[250px]">
+                <figure className="p-7 bg-green-100 w-[100px] h-[100px] mx-auto rounded-lg">
+              <img src="https://cdn.icon-icons.com/icons2/154/PNG/512/men_22005.png" alt="Shoes" className="rounded-xl" />
+                     </figure>
+                <div className="card-body items-center text-center">
+                    <p className='font-serif text-3xl'>{ data?.followers}</p>
+                    <h3 className='font-bold'>followers</h3>
+                         </div>
+                </div>
+                <div className="card bg-base-100 shadow-xl w-[250px] h-[250px]">
+                <figure className="p-7 bg-green-100 w-[100px] h-[100px] mx-auto rounded-lg">
+              <img src="https://cdn.icon-icons.com/icons2/154/PNG/512/men_22005.png" alt="Shoes" className="rounded-xl" />
+                     </figure>
+                <div className="card-body items-center text-center">
+                    <p className='font-serif text-3xl'>{ data?.followers}</p>
+                    <h3 className='font-bold'>followers</h3>
+                         </div>
+                </div>
+                <div className="card bg-base-100 shadow-xl w-[250px] h-[250px]">
+                <figure className="p-7 bg-green-100 w-[100px] h-[100px] mx-auto rounded-lg">
+              <img src="https://cdn.icon-icons.com/icons2/154/PNG/512/men_22005.png" alt="Shoes" className="rounded-xl" />
+                     </figure>
+                <div className="card-body items-center text-center">
+                    <p className='font-serif text-3xl'>{ data?.followers}</p>
+                    <h3 className='font-bold'>followers</h3>
+                         </div>
+                </div>
+                
+
+           </div>
+        </div>
+    );
+};
+
+export default StatList;
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Components/Hero';
 import WorkList from './Components/WorkList';
+import StatList from './Components/StatList';
 
 const page = () => {
   return (
     <div>
       <Hero></Hero>
       <WorkList></WorkList>
+      <StatList></StatList>
     </div>
   );
 };
