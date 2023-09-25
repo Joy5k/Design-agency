249ca12f13396d5e68e5470b015e45fd9d72d794
Joy <mmehedihasanjoyv@gmail.com>
2023-09-26 00:14:41 +0600

    added the team part successfully

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
@@ -10,28 +11,28 @@ const Navbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Home</a></li>
+        <li><Link href="/" >Home</Link></li>
        
-        <li><a>Team</a></li>
-        <li><a>Service</a></li>
-        <li><a>Projects</a></li>
-        <li><a>Testimonial</a></li>
+        <li><Link href="/" >Team</Link></li>
+        <li><Link href="/" >Service</Link></li>
+        <li><Link href="/" >Projects</Link></li>
+        <li><Link href="/" >Testimonial</Link></li>
       </ul>
     </div>
-    <a className="btn btn-ghost normal-case text-xl font-bold"><span className='-mr-2'>Design</span><span className='text-red-500'>Agency</span></a>
+    <Link href="/"  className="btn btn-ghost normal-case text-xl font-bold"><span className='-mr-2'>Design</span><span className='text-red-500'>Agency</span></Link>
   </div>
   <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li className='hover:text-[#20B15A] hover:bg-transparent  hover:border-[#20B15A] hover:font-semi-bold '><a className='hover:text-[#20B15A] hover:border-[#20B15A]'>Home</a></li>
-       <li><a className='hover:text-[#20B15A] hover:border-[#20B15A]'>Service</a></li>
-       <li><a className='hover:text-[#20B15A] hover:border-[#20B15A]'>Team</a></li>
-       <li><a className='hover:text-[#20B15A] hover:border-[#20B15A]'>Projects</a></li>
-       <li><a className='hover:text-[#20B15A] hover:border-[#20B15A]'>Testimonial</a></li>
+                        <li className='hover:text-[#20B15A] hover:bg-transparent  hover:border-[#20B15A] hover:font-semi-bold '><Link href="/"  className='hover:text-[#20B15A] hover:border-[#20B15A]'>Home</Link></li>
+       <li><Link  href="/Pages/service" className='hover:text-[#20B15A] hover:border-[#20B15A]'>Service</Link></li>
+       <li><Link href="/Pages/team"  className='hover:text-[#20B15A] hover:border-[#20B15A]'>Team</Link></li>
+       <li><Link href="/"  className='hover:text-[#20B15A] hover:border-[#20B15A]'>Projects</Link></li>
+       <li><Link href="/"  className='hover:text-[#20B15A] hover:border-[#20B15A]'>Testimonial</Link></li>
      </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn border border-[#20B15A] hover:bg-[#20B15A] hover:text-white">Login</a>
-    <a className="btn ml-2 bg-[#20B15A] text-white hover:bg-white hover:text-[#20B15A] hover:border-[#20B15A]">Registration</a>
+    <Link href="/"  className="btn border border-[#20B15A] hover:bg-[#20B15A] hover:text-white">Login</Link>
+    <Link href="/"  className="btn ml-2 bg-[#20B15A] text-white hover:bg-white hover:text-[#20B15A] hover:border-[#20B15A]">Registration</Link>
   </div>
 </div>
         </div>
---
+++ src/app/Pages/team/page.jsx
@@ -0,0 +1,67 @@
+'use client'
+import React, { useEffect, useState } from 'react';
+import { FaFacebookF } from "react-icons/fa";
+import { HiArrowSmRight } from "react-icons/hi";
+import { BsInstagram, BsTwitter } from "react-icons/bs";
+import Link from 'next/link';
+
+const page = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch('https://agency.teamrabbil.com/api/TeamList')
+            .then(res => res.json())
+            .then(data => {
+              setData(data);
+              console.log(data,'team list');
+            })
+    },[])
+    return (
+      <div className='px-[105px] py-67px bg-white mb-36'>
+        <div className=' pb-20 pt-5 w'>
+          <h1 className='text-3xl font-bold my-16'>Team</h1>
+          <div className='flex align-middle'>
+
+            <Link className='inline' href='/'>Home
+            
+            </Link>
+            <span className='mt-1 mx-3'><HiArrowSmRight></HiArrowSmRight></span>
+          <Link className='text-green-500' href='/'>Team</Link>
+          </div>
+        </div>
+                <div className='mb-10'>
+                <p className='text-xl font-semibold text-green-500'>Our Team Member</p>
+                <h3 className='font-bold text-3xl'>Check our awesome team <br /> members</h3>
+            </div>
+        {/* cart items */}
+        
+        <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8'>
+        {
+  data.map((team) => (
+    <div key={team.id} className="card w-fit bg-transparent p-2 shadow-sm" >
+      <figure><img src={team.image} alt="Shoes" /></figure>
+      <div className= 'w-[150px] mx-auto -mt-12 mb-10 bg-slate-200 p-1 rounded-3xl flex justify-evenly'>
+        <Link href={team?.facebook} className='cursor-pointer hover:bg-blue-600 hover:text-blue-50 p-2 hover:rounded-full'><FaFacebookF></FaFacebookF></Link>
+<Link href={team?.twitter}  className='cursor-pointer hover:bg-sky-400 hover:text-blue-50 p-2 hover:rounded-full'><BsTwitter></BsTwitter></Link>
+<Link href={team?.instagram}  className='cursor-pointer hover:bg-red-600 hover:text-blue-50 p-2 hover:rounded-full'><BsInstagram></BsInstagram></Link>
+      </div>
+
+      <div className="mt-2 text-start">
+        <p className='text-start text-2xl font-semibold'>{ team?.name}</p>
+        <p className='text-start'>{team?.bio }</p>
+      </div>
+    </div>
+  ))
+}
+        </div> 
+
+
+
+
+
+
+
+        </div>
+    );
+};
+
+export default page;
