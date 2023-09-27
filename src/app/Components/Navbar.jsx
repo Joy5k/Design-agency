79e30cd27f7ee454ffbc42c19ae276d147f24c98
Joy <mmehedihasanjoyv@gmail.com>
2023-09-27 16:23:14 +0600

    added the link on navbar for mobile responsive

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -11,19 +11,18 @@ const Navbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><Link href="/" >Home</Link></li>
-       
-        <li><Link href="/" >Team</Link></li>
-        <li><Link href="/" >Service</Link></li>
-        <li><Link href="/" >Projects</Link></li>
-        <li><Link href="/" >Testimonial</Link></li>
+      <li className='hover:text-[#20B15A] hover:bg-transparent  hover:border-[#20B15A] hover:font-semi-bold '><Link href="/"  className='hover:text-[#20B15A] hover:border-[#20B15A]'>Home</Link></li>
+       <li><Link  href="/Pages/service" className='hover:text-[#20B15A] hover:border-[#20B15A]'>Service</Link></li>
+       <li><Link href="/Pages/team"  className='hover:text-[#20B15A] hover:border-[#20B15A]'>Team</Link></li>
+       <li><Link href="/Pages/Allprojects"  className='hover:text-[#20B15A] hover:border-[#20B15A]'>Projects</Link></li>
+       <li><Link href="/Pages/Testimonial"  className='hover:text-[#20B15A] hover:border-[#20B15A]'>Testimonial</Link></li>
       </ul>
     </div>
     <Link href="/"  className="btn btn-ghost normal-case text-xl font-bold"><span className='-mr-2'>Design</span><span className='text-red-500'>Agency</span></Link>
   </div>
   <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li className='hover:text-[#20B15A] hover:bg-transparent  hover:border-[#20B15A] hover:font-semi-bold '><Link href="/"  className='hover:text-[#20B15A] hover:border-[#20B15A]'>Home</Link></li>
+        <li className='hover:text-[#20B15A] hover:bg-transparent  hover:border-[#20B15A] hover:font-semi-bold '><Link href="/"  className='hover:text-[#20B15A] hover:border-[#20B15A]'>Home</Link></li>
        <li><Link  href="/Pages/service" className='hover:text-[#20B15A] hover:border-[#20B15A]'>Service</Link></li>
        <li><Link href="/Pages/team"  className='hover:text-[#20B15A] hover:border-[#20B15A]'>Team</Link></li>
        <li><Link href="/Pages/Allprojects"  className='hover:text-[#20B15A] hover:border-[#20B15A]'>Projects</Link></li>
